89cc44e83ab9176318699f4cff3d423c68e12e55
milangeorge2000 <45516866+milangeorge2000@users.noreply.github.com>
2020-08-15 20:49:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,10 @@
 from flask import Flask,render_template,request
 import pickle
-import re
-import nltk
-nltk.download('stopwords')
-from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
 
 model = pickle.load(open('sgd_gs.pkl','rb'))
 tf = pickle.load(open('tf_sentimental.pkl','rb'))
 
-stop_words = set(stopwords.words('english'))
-stop_words.remove('very')
-stop_words.remove('not')
-stop_words.remove("isn't")
-stop_words.remove("doesn't")
-stop_words.remove("too")
-stop_words.remove('most')
-
-
-
-ps = PorterStemmer()
-
-
-def preprocessing(a):
-      a = re.sub('[^a-zA-Z]',' ',a)
-      a = a.lower()
-      a = a.split()
-    
-      message = [ps.stem(word) for word in a if not word in stop_words and len(word) >1]
-      message = ' '.join(message)
-      print(message)
-      
-      transformed = tf.transform([message])
-      return transformed
-
-
-
+from preprocess import preprocessing
 
 
 app = Flask(__name__)
